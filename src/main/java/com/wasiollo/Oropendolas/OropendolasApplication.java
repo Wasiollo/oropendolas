aaf681d620e5c26e627f9292409b86b4ee7ed0f4
mwasiak <mwasiak@bluesoft.net.pl>
2019-06-13 13:17:44 +0200

    Initial commit

---
+++ src/main/java/com/wasiollo/Oropendolas/OropendolasApplication.java
@@ -0,0 +1,13 @@
+package com.wasiollo.Oropendolas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OropendolasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OropendolasApplication.class, args);
+	}
+
+}
